0b1302d5b86e09129f00e3457d84efc22307d0b2
Ainaa Azman <ainaasakinah@gmail.com>
2024-03-31 11:52:11 +0900

    add hard coded ar generation screens

--- src/Contex/FileContext.jsx
+++ src/Contex/FileContext.jsx
@@ -4,26 +4,36 @@ const FileContext = createContext();
 
 const FileProvider = ({ children }) => {
   const [file, setFile] = useState(null);
-  //   const [imageResponse, setImageResponse] = useState(null);
-  const [imageURL, setImageURL] = useState(null);
+
+  // Hard coded for testing - store card file for AR prompt
+  const [cardImage, setCardImage] = useState(
+    "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/card.png"
+  );
+
+  // Hard coded for testing - store AR mind file for image targeting
+  const [mindFile, setMindFile] = useState("./bath.mind");
 
   const setUploadedFile = (newFile) => {
     setFile(newFile);
   };
 
-  //   const setImageResponseData = (newImageData) => {
-  //     setImageResponse(newImageData);
-  //   };
+  const setUploadedCardImage = (newUploadedImage) => {
+    setCardImage(newUploadedImage);
+  };
+
+  const setGeneratedMindFile = (newMindFile) => {
+    setMindFile(newMindFile);
+  };
 
   return (
     <FileContext.Provider
       value={{
         file,
         setUploadedFile,
-        // imageResponse,
-        // setImageResponseData,
-        imageURL,
-        setImageURL,
+        cardImage,
+        setUploadedCardImage,
+        mindFile,
+        setGeneratedMindFile,
       }}
     >
       {children}
---
+++ src/components/MindScreen/MindScreen.jsx
@@ -0,0 +1,121 @@
+import React, { useContext, useState } from "react";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import { FileContext } from "../../Contex/FileContext";
+import { useNavigate } from "react-router-dom";
+
+const Input = styled("input")({
+  display: "none",
+});
+
+const MindScreen = () => {
+  const { setUploadedCardImage, cardImage, setGeneratedMindFile } =
+    useContext(FileContext);
+  const [uploadStatus, setUploadStatus] = useState("");
+  const navigation = useNavigate();
+
+  const handleFileChange = (event) => {
+    setUploadedCardImage(event.target.files[0]);
+    setUploadStatus(""); // Reset upload status message on new file selection
+  };
+
+  const handleUploadClick = async () => {
+    if (!cardImage) {
+      setUploadStatus("No file selected.");
+      return;
+    }
+
+    // prepare form data to be sent to BE
+
+    // navigate to AR screen, you have to set once BE resonse is succeeded
+    navigation("/aframe", {
+      state: { cardImage },
+    });
+
+    return;
+
+    try {
+      setUploadStatus("Uploading...");
+      const response = await fetch(
+        "https://0ebd-152-165-122-193.ngrok-free.app/create-mind-file",
+        {
+          // Insert Url here
+          method: "POST",
+          body: cardImage,
+          header: {
+            "Content-Type": "image/jpeg",
+          },
+        }
+      );
+      if (response.ok) {
+        console.log("Response", response);
+        const result = await response.body;
+
+        // const reader = new FileReader();
+
+        // reader.onload = ()=>
+        console.log(result);
+
+        setUploadStatus("Upload successful!");
+        // get mind from BE response should have .mind file
+        // you should extract .mind file
+        // if necessary you have to convert stream to object
+        // store .mind file in context
+
+        setGeneratedMindFile("you must set the mind file here");
+
+        // Process the response as needed
+
+        console.log(result);
+
+        navigation("/aframe", {
+          state: { cardImage },
+        });
+      } else {
+        setUploadStatus("Upload failed.");
+      }
+    } catch (error) {
+      console.error("Error uploading the file:", error);
+      setUploadStatus("Upload failed. Please try again.");
+    }
+  };
+
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <Typography variant="h4" gutterBottom>
+        Upload an Image
+      </Typography>
+      <label htmlFor="icon-button-file">
+        <Input
+          accept="image/*"
+          id="icon-button-file"
+          type="file"
+          onChange={handleFileChange}
+        />
+        <IconButton
+          color="primary"
+          aria-label="upload picture"
+          component="span"
+        >
+          <PhotoCamera />
+        </IconButton>
+      </label>
+      {cardImage && <Typography>File selected: {cardImage.name}</Typography>}
+      <div style={{ marginTop: "20px" }}>
+        <Button
+          variant="contained"
+          component="span"
+          onClick={handleUploadClick}
+        >
+          Upload
+        </Button>
+      </div>
+      {uploadStatus && <Typography>{uploadStatus}</Typography>}
+    </div>
+  );
+};
+
+export default MindScreen;
